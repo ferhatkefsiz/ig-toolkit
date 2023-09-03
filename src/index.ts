4e90d81169f2bd63fd6c8695fbe4b2d53b6b355e
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2023-09-03 23:21:09 +0300

    refactor: entry point

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,63 @@
-import dotenv from "dotenv"
+import inquirer from "inquirer"
 import figlet from "figlet"
 import chalk from "chalk"
+import dotenv from "dotenv"
 
 import { login } from "./modules/login"
 import { logout } from "./modules/logout"
-import { prompt } from "./helpers/prompt"
+import { fetchFollowers } from "./modules/followers"
 
-// Load environment variables from the .env file
 dotenv.config()
 
-// IIFE  https://developer.mozilla.org/en-US/docs/Glossary/IIFE
-;(async () => {
+async function main() {
   console.log(chalk["red"](figlet.textSync("ig-toolkit", { horizontalLayout: "universal smushing" })))
 
-  const username = process.env.IG_USERNAME || ""
-  const password = process.env.IG_PASSWORD || ""
+  const credentials = await inquirer.prompt([
+    {
+      name: "username",
+      type: "input",
+      message: "Enter your username:",
+      validate: function (value) {
+        if (value.length) {
+          return true
+        } else {
+          return "Please enter your username."
+        }
+      },
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "Enter your password:",
+      mask: "*",
+      validate: function (value) {
+        if (value.length >= 8) {
+          return true
+        } else {
+          return "Password must be at least 8 characters long."
+        }
+      },
+    },
+  ])
 
-  await login(username, password)
+  await login(credentials.username, credentials.password)
 
-  await prompt().then((res) => {
-    if (res.CHOICE_OPTIONS === "GET_INFO") {
-      console.log("getting info account")
-    }
-  })
+  const choice = await inquirer.prompt([
+    {
+      name: "CHOICE_OPTIONS",
+      type: "list",
+      message: "Select your option",
+      choices: [{ name: "Get Followers", value: "GET_FOLLOWERS" }],
+    },
+  ])
+
+  switch (choice.CHOICE_OPTIONS) {
+    case "GET_FOLLOWERS":
+      await fetchFollowers()
+      break
+  }
 
   await logout()
-})()
+}
+
+main()
