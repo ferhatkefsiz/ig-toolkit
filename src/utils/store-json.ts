b378c1b9dd74340c841e60e9d5fd6ae0a6c771fc
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2023-09-10 21:40:33 +0300

    refactor: storeJSON util

--- src/utils/store-json.ts
+++ src/utils/store-json.ts
@@ -1,18 +1,16 @@
+import chalk from "chalk"
 import fs from "fs"
 
-/**
- * @param filePath - The path to the JSON file.
- * @param data - The data to be written to the file.
- * @returns Promise<void>
- */
-export async function storeToJSON(filePath: string, data: any): Promise<void> {
+export async function storeJSON(filePath: string, data: any): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     const jsonData = JSON.stringify(data, null, 2)
 
     fs.writeFile(filePath, jsonData, (err) => {
       if (err) {
+        console.error(`${chalk.red("Error writing to " + filePath)}: ${err.message}`)
         reject(err)
       } else {
+        console.log(`Data written to ${filePath}`)
         resolve()
       }
     })
