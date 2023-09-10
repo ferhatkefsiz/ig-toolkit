35ccbfb4962cd9e3c20a51b0c9db49ebb3dea6bd
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2023-09-10 21:41:39 +0300

    refactor: storeJSON usage

--- src/modules/followings.ts
+++ src/modules/followings.ts
@@ -1,11 +1,8 @@
 import { ig } from "./login"
-import chalk from "chalk"
-import { storeToJSON } from "../utils/store-json"
-import { createDirectory } from "../utils/create-directory"
+import { storeJSON } from "../utils/store-json"
 
 export async function fetchFollowings(): Promise<void> {
   try {
-    await createDirectory("./dist/data")
     const followingsFeed = ig.feed.accountFollowing()
 
     let allFollowings: any = []
@@ -20,13 +17,7 @@ export async function fetchFollowings(): Promise<void> {
 
     const usernames = allFollowings.map((following: any) => `https://www.instagram.com/${following.username}`)
 
-    storeToJSON("./dist/data/followings.json", usernames)
-      .then(() => {
-        console.log(chalk.green("Data written to ./dist/data/followings.json"))
-      })
-      .catch((err) => {
-        console.error(`${chalk.red("Error writing to ./dist/data/followings.json")}: ${err.message}`)
-      })
+    storeJSON("./data/followings.json", usernames)
   } catch (error: any) {
     console.error("Operation failed:", error.message)
   }
